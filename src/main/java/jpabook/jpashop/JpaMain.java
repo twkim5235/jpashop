6e83f0c8378a36e2dc74b38978c0e043d8ae9457
twkim5235 <sssunggure@naver.com>
2021-11-10 10:19:03 +0900

    message

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); //트랜잭션 시작
+
+        try {
+            Order order = new Order();
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+
+            tx.commit(); //트랜잭션 커밋
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        em.close();
+
+        emf.close();
+    }
+}
