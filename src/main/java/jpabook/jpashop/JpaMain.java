6977b01802e9d6f02ed59e9bc79398b893c5f900
twkim5235 <sssunggure@naver.com>
2022-02-13 00:39:15 +0900

    (FIX) 상속관계 매핑 추가, MappedClass를 통한 BaseEntity 추가

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -19,13 +21,11 @@ public class JpaMain {
         tx.begin(); //트랜잭션 시작
 
         try {
-            Order order = new Order();
-            em.persist(order);
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
 
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-
-            em.persist(orderItem);
+            em.persist(book);
 
             tx.commit(); //트랜잭션 커밋
         } catch (Exception e){
---
+++ src/main/java/jpabook/jpashop/domain/BaseEntity.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    private String createdBy;
+    private LocalDateTime createdDate;
+    private String lasModifiedBy;
+    private LocalDateTime lastModifiedDate;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLasModifiedBy() {
+        return lasModifiedBy;
+    }
+
+    public void setLasModifiedBy(String lasModifiedBy) {
+        this.lasModifiedBy = lasModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
